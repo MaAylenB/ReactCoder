8334eb0b87bcfcc557e0fa5f34644cf0900c4fc2
Maria Aylen <maaylenbenavidez@gmail.com>
2024-03-26 00:59:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
-import logo from './aimelogo2.jpeg';
+import logo from './logo.svg';
 import './App.css';
 
-const NavBar = () => {
-    return (
-      <nav>
-        <div className="logo">
-          <img src={logo} alt="Logo de la tienda react" />
-        </div>
-        <ul className="menu">
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/productos">Productos</a></li>
-           <ul>
-                    <li><a href="">Carteras</a></li> 
-                    <li><a href="">Indumentaria</a></li> 
-                    <li><a href="">Anillos</a></li> 
-                    <li><a href="">Collares</a></li> 
-                    <li><a href="">Pulseras</a></li> 
-           </ul>
-          <li><a href="/contacto">Contacto</a></li>
-        </ul>
-        <div className="cart-widget">
-          {/* Widget del carrito de compras */}
-          <span>Carrito</span>
-        </div>
-      </nav>
-    );
-  };
-  
-  // Componente Contenedor para mostrar el catálogo de productos
-  const Contenedor = (props) => {
-    return (
-      <div>
-        <h1>Bienvenido a {props.tienda}</h1>
-        <p>{props.mensaje}</p>
-        {/*Lógica para mostrar el catálogo de productos */}
-      </div>
-    );
-  };
-  
-  // Componente principal
-  const App = () => {
-    return (
-      <div>
-        <NavBar />
-        <Contenedor tienda="Aime accesorios" mensaje="¡Gracias por su visita!" />
-      </div>
-    );
-  };
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
-
