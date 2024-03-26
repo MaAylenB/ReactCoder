4325f206bb2d2d51506927d2fd6fab320c76643d
Maria Aylen <maaylenbenavidez@gmail.com>
2024-03-26 10:38:48 -0300

    arreglos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './aimelogo2.jpeg';
+import './index.css';
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ import CartWidget from './CartWidget'
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <Link to="/" className="logo"><h1>Carpishop</h1></Link>
+        <Link to="/" className="logo"><h1>Aimé Accesorios</h1></Link>
         <ul className="menu">
             <li><Link className="menu-link" to="/">Inicio</Link></li>
             <li><Link className="menu-link" to="/productos">Productos</Link></li>
-            <li><Link className="menu-link" to="/productos/medias">Medias</Link></li>
-            <li><Link className="menu-link" to="/productos/pantalones">Pantalones</Link></li>
-            <li><Link className="menu-link" to="/productos/remeras">Remeras</Link></li>
-            <li><Link className="menu-link" to="/productos/buzos">Buzos</Link></li>
+            <li><Link className="menu-link" to="/productos/anillos">Anillos</Link></li>
+            <li><Link className="menu-link" to="/productos/collares">Collares</Link></li>
+            <li><Link className="menu-link" to="/productos/cartera">Carteras</Link></li>
+            <li><Link className="menu-link" to="/productos/pulseras">Pulseras</Link></li>
             <li><Link className="menu-link" to="/nosotros">Nosotros</Link></li>
             <li><Link className="menu-link" to="/contacto">Contacto</Link></li>
             <li><CartWidget /></li>
